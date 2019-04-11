befaacc33b95170f596c9de9ea4fe434a90b8449
AlanHoNCKU <mylifeai1116@gmail.com>
2019-04-11 16:35:38 +0800

    rename

--- main.py
+++ main.py
@@ -1,38 +1,140 @@
 # -*- coding: UTF-8 -*-
 """
-Date:2019/04/01
+Date:2019/04/11
 Author:Chia Yu,Ho
 """
-import airsim
-import os
 import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
 from PIL import Image
+from skimage import transform,data
 
-def init_drone_pos():
-    global client
-    client = airsim.MultirotorClient()
-    client.reset()
-    client.confirmConnection()
-    client.enableApiControl(True)
-    client.armDisarm(True)
-    position = airsim.Vector3r(113.7,70.2,-0.5)
-    heading = airsim.utils.to_quaternion(0, 0, 360)
-    pose = airsim.Pose(position, heading)
-    client.simSetVehiclePose(pose, True)
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+from object_detection.utils import ops as utils_ops
 
-def get_img():
-    responses = client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.Scene, False, False)])
-    response = responses[0]
-    # get numpy array
-    img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) 
-    # reshape array to 4 channel image array H X W X 4
-    img_rgba = img1d.reshape(response.height, response.width, 4)
-    # 垂直翻轉圖像
-    img_rgba = np.flipud(img_rgba)
-    # 寫入成 png
-    airsim.write_png(os.path.normpath('img_rgb' + '.png'), img_rgba)
-    image = Image.open('img_rgb.png')
-    image.load() # required for png.split()
-    background = Image.new("RGB", image.size, (255, 255, 255))
-    background.paste(image, mask=image.split()[3]) # 3 is the alpha channel
-    background.save('img_rgb.png', 'PNG', quality=100)
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+
+"""
+設定模型
+"""
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+"""
+下載模型
+"""
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+  file_name = os.path.basename(file.name)
+  if 'frozen_inference_graph.pb' in file_name:
+    tar_file.extract(file, os.getcwd())
+
+"""
+載入模型到memory
+"""
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[1], image.shape[2])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: image})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+image = Image.open('img_rgb.png')
+plt.imshow(image)
+plt.show()
+image_np = load_image_into_numpy_array(image)
+image_np_expanded = np.expand_dims(image_np, axis=0)
+output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+vis_util.visualize_boxes_and_labels_on_image_array(
+    image_np,
+    output_dict['detection_boxes'],
+    output_dict['detection_classes'],
+    output_dict['detection_scores'],
+    category_index,
+    instance_masks=output_dict.get('detection_masks'),
+    use_normalized_coordinates=True,
+    line_thickness=8)
+# IMAGE_SIZE = (12, 8)
+# plt.figure(figsize=IMAGE_SIZE)
+plt.imshow(image_np)
+# print(image_np)
+plt.show()
